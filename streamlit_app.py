0adf71cdc2ce7804b6dee1fe717451d75fc2d1cd
ahospedales <160843764+ahospedales@users.noreply.github.com>
2024-02-26 15:50:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#allow enduser to add another fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('tanks for adding ', fruit_choice)
