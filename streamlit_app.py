b4f76ea3ec78995b7aa2340ce66a965f1e651e0c
ahospedales <160843764+ahospedales@users.noreply.github.com>
2024-02-26 13:44:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
 
-im`prt snpwflake.connector
+import snpwflake.connector
